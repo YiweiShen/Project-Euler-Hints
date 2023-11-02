5bb6957e7045cd7b9867600732eab67d0e72e1fc
Yiwei Shen <YiWeiShen@users.noreply.github.com>
2023-11-01 23:06:12 -0400

    add p836

---
+++ 800-850/p836.py
@@ -0,0 +1,4 @@
+bolded_words = "affine plane radically integral local field open oriented line section jacobian orthogonal kernel embedding"
+result = "".join([i[0] for i in bolded_words.split()])
+
+print(result)
